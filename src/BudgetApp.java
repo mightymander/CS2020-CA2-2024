77a8dfa6985bb211fa9495b322744a4e4387baac
mightymander <jacobdcorbett86@gmail.com>
2024-11-18 15:46:57 +0000

    refactor: remove BudgetUtils, BudgetApp, and Budget classes to streamline codebase

--- src/BudgetApp.java
+++
@@ -1,7 +0,0 @@
-package src;
-
-public class BudgetApp {
-    public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(() -> ui.BudgetPanel.createAndShowGUI());
-    }
-}
